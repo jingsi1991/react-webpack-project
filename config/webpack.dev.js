69c9a8a1b9c77d353a31f6e0b3c87ac083e0a784
liudawei <liudawei@icsoc.net>
2021-03-23 09:57:05 +0800

    lesson-0：webpack-dev-server存在兼容性，改用webpack serve启动

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,15 +1,15 @@
 // webpack.dev.js 开发环境的配置
-const {merge} = require('webpack-merge');
-const baseWebpackConfig = require('./webpack.base');
-const config = require('./config');
+const { merge } = require("webpack-merge");
+const baseWebpackConfig = require("./webpack.base");
+const config = require("./config");
 
 module.exports = merge(baseWebpackConfig, {
-  mode: 'development',
+  mode: "development",
   output: {
-    filename: 'js/[name].[hash:8].js',
-    publicPath: config.publicPath // 这里可以省略
+    filename: "js/[name].[hash:8].js",
+    publicPath: config.publicPath, // 这里可以省略
   },
   module: {
-    rules: []
+    rules: [],
   },
-})
+});
